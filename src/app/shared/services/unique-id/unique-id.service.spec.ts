b88adfeb7d22cabfe5ea716334200068ae178efc
luizfiller <luiz.fsilva@sptech.school>
2023-04-13 11:43:02 -0300

    Add - beforeEach() to instances + tests for getNumber method

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,15 +7,26 @@ import { UniqueIdService } from "./unique-id.service";
 // });
 
 describe(UniqueIdService.name, () => {
+
+  let service = new UniqueIdService();
+  /*
+  beforeEach() -> antes de executar cada teste,
+  o que estiver dentro da arrow-function será executado
+  para cada um dos casos de testes
+  */
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
   /*
   Dentro do 'it()' deve ser especificada em string
   a regra esperada para determinada função usando
   o seguinte padrão:
   '<nome_função> should <ação_esperada> when <evento/situação>'
   */
-  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     // Instancia uma variável com o serviço ou component a ser testado
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
 
     /*
@@ -27,17 +38,35 @@ describe(UniqueIdService.name, () => {
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-
-  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
     // Set() -> não aceita valores iguais
     const ids = new Set();
 
     for (let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
-
     }
     expect(ids.size).toBe(50);
   });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generatedIds when called`, () => {
+    const ids = new Set();
+
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should throw when called with empty`, () => {
+      const emptyValues = [null, undefined, ''];
+
+      emptyValues.forEach(emptyValue =>
+        expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow()
+      );
+
+    })
 });
 
